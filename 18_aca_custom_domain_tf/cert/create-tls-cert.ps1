87030c30565705feeb6d138a178469dc478044b0
Thiruazure2lab <Thiruazure2lab@gmail.com>
2024-08-18 19:54:38 +0530

    MODIFIEDS THIRUAWS.COM

--- 18_aca_custom_domain_tf/cert/create-tls-cert.ps1
+++ 18_aca_custom_domain_tf/cert/create-tls-cert.ps1
@@ -1,20 +1,20 @@
 # Create a self-signed SSL certificate to use with the Ingress
-openssl req -new -x509 -nodes -out aca-app.com.crt -keyout aca-app.com.key -subj "/CN=aca-app.com" -addext "subjectAltName=DNS:aca-app.com"
+openssl req -new -x509 -nodes -out thiruaws.com.crt -keyout thiruaws.com.key -subj "/CN=thiruaws.com" -addext "subjectAltName=DNS:thiruaws.com"
 
 # Export the SSL certificate
-openssl pkcs12 -export -in aca-app.com.crt -inkey aca-app.com.key -out aca-app.com.pfx
+openssl pkcs12 -export -in thiruaws.com.crt -inkey thiruaws.com.key -out thiruaws.com.pfx
 
 # Password for the PFX file: @Aa123456789
 
-cat aca-app.com.crt > aca-app.com.pem
-cat aca-app.com.key >> aca-app.com.pem
+cat thiruaws.com.crt > thiruaws.com.pem
+cat thiruaws.com.key >> thiruaws.com.pem
 
 
 
 
 openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes `
-  -keyout poorclaresarundel.org.key -out poorclaresarundel.org.crt -subj "/CN=aca-app.com" `
-  -addext "subjectAltName=DNS:aca-app.com,DNS:www.aca-app.com,IP:8.8.8.8"
+  -keyout poorclaresarundel.org.key -out poorclaresarundel.org.crt -subj "/CN=thiruaws.com" `
+  -addext "subjectAltName=DNS:thiruaws.com,DNS:www.thiruaws.com,IP:8.8.8.8"
 
 # sudo chmod +r poorclaresarundel.org.key
 cat poorclaresarundel.org.crt > poorclaresarundel.org.pem
